e9a0ae1ac6973d5b5677f63903b8f2e4ff11346e
cyferdev <mrnzero321@gmail.com>
2023-09-03 01:29:55 +0530

    m

--- app.py
+++ app.py
@@ -1,1 +1,48 @@
 from dotenv import load_dotenv
+import os
+from PyPDF2 import PdfReader
+import streamlit as st
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.llms import OpenAI
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.callbacks import get_openai_callback
+
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Chatbot for your pdf")
+    st.header("Ask your pdf")
+
+    pdf = st.file_uploader("Upload your pdf", type="pdf")
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+        text_splitter = CharacterTextSplitter(
+            separator="\n",
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len,
+        )
+
+        chunks = text_splitter.split_text(text)
+        embeddings = OpenAIEmbeddings()
+        knowledge_base = FAISS.from_texts(chunks, embeddings)
+        user_question = st.text_input("Enter the question you want to ask")
+        if user_question:
+            docs = knowledge_base.similarity_search(user_question)
+            llm = OpenAI()
+            chain = load_qa_chain(llm, chain_type="stuff")
+
+            with get_openai_callback() as cb:
+                result = chain.run(input_documents=docs,
+                                   question=user_question)
+                st.write(result)
+                print(cb)
+
+
+if __name__ == "__main__":
+    main()
